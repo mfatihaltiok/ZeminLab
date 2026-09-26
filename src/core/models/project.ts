43ecf8b24d8943f45cea8b898f3fa19557008066
mfa38 <m.fatihaltiok@gmail.com>
2026-09-26 16:50:19 +0300

    feat: track post-liquefaction evidence

--- src/core/models/project.ts
+++ src/core/models/project.ts
@@ -61,6 +61,8 @@ export interface FoundationParameters{
   foundationInterface?:FoundationInterface
   seismicBelowGroundwater?:boolean
   cyclicSettlementAnalysisCompleted?:boolean
+  postLiquefactionAssessmentCompleted?:boolean
+  jetGroutQualityControlCompleted?:boolean
   nonlinearSoilDeformationAnalysisCompleted?:boolean
   tallBuilding?:boolean
 }
@@ -79,7 +81,7 @@ export const defaultProjectInfo:ProjectInfo={
   soilParameters:{unitWeight:0,saturatedUnitWeight:0,cohesion:0,frictionAngle:0,undrainedCohesion:undefined,groundwaterDepth:undefined,surfaceSlope:0,foundationBaseSlope:0,finesContent:0,liquefactionContinuousOrThickLens:undefined,classification:{system:'TS EN ISO 14688-2'}},
   foundationParameters:{
     foundationType:'tekil',footingWidth:0,footingLength:0,footingDepth:0,safetyFactor:3,verticalLoad:0,horizontalLoad:0,momentX:0,momentY:0,
-    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined,cyclicSettlementAnalysisCompleted:undefined,nonlinearSoilDeformationAnalysisCompleted:undefined,tallBuilding:undefined
+    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined,cyclicSettlementAnalysisCompleted:undefined,postLiquefactionAssessmentCompleted:undefined,jetGroutQualityControlCompleted:undefined,nonlinearSoilDeformationAnalysisCompleted:undefined,tallBuilding:undefined
   },
   jetGrout:{layout:'square'},
   visualDocuments:{}
