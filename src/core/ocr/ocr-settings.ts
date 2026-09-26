d564d6337e3e7f7fa472c9926ff35c74d484f80f
mfa38 <m.fatihaltiok@gmail.com>
2026-09-26 04:39:44 +0300

    chore: remove abandoned OCR settings

--- src/core/ocr/ocr-settings.ts
+++
@@ -1,3 +0,0 @@
-export type OcrProvider = 'local' | 'remote'
-export interface OcrSettings { provider: OcrProvider; endpoint: string; apiKey?: string }
-export interface OcrPublicSettings { provider: OcrProvider; endpoint: string; hasApiKey: boolean }
