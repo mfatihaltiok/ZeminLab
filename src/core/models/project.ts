1e3677ac180f86979e3a270298f18c0632cd6615
mfa38 <m.fatihaltiok@gmail.com>
2026-09-26 16:19:18 +0300

    feat: track seismic soil deformation analysis completion

--- src/core/models/project.ts
+++ src/core/models/project.ts
@@ -60,6 +60,8 @@ export interface FoundationParameters{
   usePassiveResistance:boolean
   foundationInterface?:FoundationInterface
   seismicBelowGroundwater?:boolean
+  cyclicSettlementAnalysisCompleted?:boolean
+  nonlinearSoilDeformationAnalysisCompleted?:boolean
 }
 
 export interface ProjectInfo{
@@ -76,7 +78,7 @@ export const defaultProjectInfo:ProjectInfo={
   soilParameters:{unitWeight:0,saturatedUnitWeight:0,cohesion:0,frictionAngle:0,undrainedCohesion:undefined,groundwaterDepth:undefined,surfaceSlope:0,foundationBaseSlope:0,finesContent:0,liquefactionContinuousOrThickLens:undefined,classification:{system:'TS EN ISO 14688-2'}},
   foundationParameters:{
     foundationType:'tekil',footingWidth:0,footingLength:0,footingDepth:0,safetyFactor:3,verticalLoad:0,horizontalLoad:0,momentX:0,momentY:0,
-    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined
+    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined,cyclicSettlementAnalysisCompleted:undefined,nonlinearSoilDeformationAnalysisCompleted:undefined
   },
   jetGrout:{layout:'square'},
   visualDocuments:{}
