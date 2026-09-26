63c4239616125d68df1e83f556f3a1ce78ac3dba
mfa38 <m.fatihaltiok@gmail.com>
2026-09-26 16:19:34 +0300

    feat: capture high-building status for foundation compliance

--- src/core/models/project.ts
+++ src/core/models/project.ts
@@ -62,6 +62,7 @@ export interface FoundationParameters{
   seismicBelowGroundwater?:boolean
   cyclicSettlementAnalysisCompleted?:boolean
   nonlinearSoilDeformationAnalysisCompleted?:boolean
+  tallBuilding?:boolean
 }
 
 export interface ProjectInfo{
@@ -78,7 +79,7 @@ export const defaultProjectInfo:ProjectInfo={
   soilParameters:{unitWeight:0,saturatedUnitWeight:0,cohesion:0,frictionAngle:0,undrainedCohesion:undefined,groundwaterDepth:undefined,surfaceSlope:0,foundationBaseSlope:0,finesContent:0,liquefactionContinuousOrThickLens:undefined,classification:{system:'TS EN ISO 14688-2'}},
   foundationParameters:{
     foundationType:'tekil',footingWidth:0,footingLength:0,footingDepth:0,safetyFactor:3,verticalLoad:0,horizontalLoad:0,momentX:0,momentY:0,
-    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined,cyclicSettlementAnalysisCompleted:undefined,nonlinearSoilDeformationAnalysisCompleted:undefined
+    resistanceFactorRv:1.4,vtX:0,vtY:0,structuralWeight:0,baseFrictionTanDelta:0.6,passiveResistanceCharacteristic:0,usePassiveResistance:false,foundationInterface:undefined,seismicBelowGroundwater:undefined,cyclicSettlementAnalysisCompleted:undefined,nonlinearSoilDeformationAnalysisCompleted:undefined,tallBuilding:undefined
   },
   jetGrout:{layout:'square'},
   visualDocuments:{}
